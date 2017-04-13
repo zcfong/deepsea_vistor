fd8ade6ffa7fb4d1aeba7c86e835a1f299b18ba0
justinfang <zhic@innapp.cn>
2017-04-13 17:24:11 +0800

    first commit

---
+++ sh/tools.sh
@@ -0,0 +1,32 @@
+apt-get install -y libssl-dev
+apt-get install -y tmux
+
+apt-get install -y build-essential
+
+apt-get install -y gcc
+
+apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libsqlite3-dev
+apt-get install -y supervisor
+
+cd /opt
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+
+redis-server &
+
+git clone https://github.com/pyenv/pyenv.git /opt/pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+
+git clone https://github.com/pyenv/pyenv-virtualenv.git /opt/pyenv/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+
+mkdir /opt/pyenv/cache
+cd /opt/pyenv/cache
+wget https://www.python.org/ftp/python/3.5.3/Python-3.5.3.tar.xz
+
+pyenv install 3.5.3
---
+++ sh/turbo.sh
@@ -0,0 +1,16 @@
+apt-get install -y linux-image-virtual grub2
+
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.3/linux-image-4.9.3-040903-generic_4.9.3-040903.201701120631_amd64.deb
+
+dpkg -i linux-image-4.9*.deb
+
+# apt-get purge -y
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+
+sysctl -p
+
+dpkg -l|grep linux-image
+
+#apt-get purge -y
+# reboot
